289c468ecf2312b1272f494e455332b06e30b4c4
Paul Revelo <prevelo01@manhattan.edu>
2020-12-11 02:41:53 +0000

    first commit

--- app.js
+++ app.js
@@ -11,25 +11,25 @@ const splitTitle = function (title) {
     let origin = "";
     let has = "";
     let wants = "";
-    
+
     if (hasIdx > wantsIdx) {
         has = title.substring(hasIdx);
         if (wantsIdx > -1) {
-            origin = title.substring(0,wantsIdx);
-            wants = title.substring(wantsIdx,hasIdx);
+            origin = title.substring(0, wantsIdx);
+            wants = title.substring(wantsIdx, hasIdx);
         } else {
-            origin = title.substring(0,hasIdx);
+            origin = title.substring(0, hasIdx);
         }
     } else if (wantsIdx > hasIdx) {
         wants = title.substring(wantsIdx);
         if (hasIdx > -1) {
-            origin = title.substring(0,hasIdx);
-            has = title.substring(hasIdx,wantsIdx);
+            origin = title.substring(0, hasIdx);
+            has = title.substring(hasIdx, wantsIdx);
         } else {
-            origin = title.substring(0,wantsIdx);
+            origin = title.substring(0, wantsIdx);
         }
     }
-    return {origin,has,wants};
+    return { origin, has, wants };
 }
 
 const client = new Client({
@@ -77,7 +77,7 @@ bot.on("ready", async () => {
     console.log("This discord bot is online!");
 
 
-    
+
     //Start listening for reddit only stuff after bot logged in.
     submissions.on("item", async (post) => {
         // Don't resend old recent posts everytime we restart the bot, since SnooStorm gets the amount specified in "limit" in SubmissionStream all at once on startup
@@ -85,7 +85,7 @@ bot.on("ready", async () => {
             submission_counter++;
             return;
         }
-        
+
         let post_title = (await post).title;
         console.log("POST: " + post_title);
         let split = splitTitle(post_title);
@@ -94,14 +94,14 @@ bot.on("ready", async () => {
         let post_has = split.has;
         let post_wants = split.wants;
 
-        if (post_has == "" && post_wants == ""){
+        if (post_has == "" && post_wants == "") {
             console.log(post_title);
             console.log("Not a Buying/Selling/Trading post.");
             return;
         }
         // Post it to the mechmarket feed channel
         bot.channels.cache.get(process.env.MM_FEED_CHANNEL_ID).send(post_title + "\n" + post.url + "\n<@&" + process.env.MM_FEED_ROLE_ID + ">");
-        
+
         client.query(query, async (err, res) => {
             if (err) {
                 console.log("Error: ");
@@ -112,15 +112,15 @@ bot.on("ready", async () => {
                 try {
                     //The bot will only message you if you're in a common server and have DMs turned on for server members
                     const user = await bot.users.fetch(row.user_id_id);
-                    
+
 
                     let alert_origin = row.origin ? row.origin : "";
                     let alert_has = row.has ? row.has : "";
                     let alert_wants = row.wants ? row.wants : "";
                     if (post_origin.includes(alert_origin) && post_has.toLowerCase().includes(alert_has.toLowerCase()) && post_wants.toLowerCase().includes(alert_wants.toLowerCase())) {
-                        let message = "**Origin:** " + (alert_origin == ""? "*Any*": alert_origin) + " **[H]** " + (alert_has == ""? "*Any*": alert_has) + " **[W]** " + (alert_wants == ""? "*Any*": alert_wants) + "\n\n" + post_title + '\n' + post.url + '\n';
+                        let message = "**Origin:** " + (alert_origin == "" ? "*Any*" : alert_origin) + " **[H]** " + (alert_has == "" ? "*Any*" : alert_has) + " **[W]** " + (alert_wants == "" ? "*Any*" : alert_wants) + "\n\n" + post_title + '\n' + post.url + '\n';
                         user.send(message);
-                        console.log("SENT MESSAGE TO: " + user.username + "#" +  user.discriminator);
+                        console.log("SENT MESSAGE TO: " + user.username + "#" + user.discriminator);
                         console.log(message);
                     }
 
